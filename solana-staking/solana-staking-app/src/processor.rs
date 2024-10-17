bc17b352e6a59d6f8aa44e056c6901b52dea6526
luisflorenz <luis.florenzan.g@gmail.com>
2024-10-17 13:02:21 +0000

    cimmit all the files

---
+++ solana-staking/solana-staking-app/src/processor.rs
@@ -0,0 +1,59 @@
+use solana_program::{
+    account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey, msg
+    };
+    use borsh::BorshDeserialize;
+    
+    use crate::instruction::Instruction;
+    use crate::error::StakingError;
+    
+    pub fn process(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+    ) -> ProgramResult {
+    let instruction = Instruction::try_from_slice(instruction_data)?;
+    
+    match instruction {
+    Instruction::Initialize {
+    rewards_per_token, } => {
+    msg!("Initialize pool");
+    process_initialize_pool(program_id, accounts, rewards_per_token)
+    }
+    _ => {
+    Err(StakingError::InvalidInstruction.into())
+    }
+    }
+    fn process_initialize_pool(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        rewards_per_token: u64,
+        ) -> ProgramResult {
+        let accounts_iter = &mut accounts.iter();
+        let signer = next_account_info(accounts_iter)?;
+        if !signer.is_signer {
+        return Err(
+        StakingError::InvalidSigner.into()); }
+        
+        let storage = next_account_info(accounts_iter)?;
+        if storage.owner != program_id {
+        return Err(StakingError::InvalidOwner.into());
+        }
+        
+        let mut storage_data = PoolStorageAccount::try_from_slice(&storage.data.borrow())?;
+        if storage_data.is_initialized() {
+        return Err(StakingError::AlreadyInitialized.into());
+        }
+        
+        storage_data.pool_authority = *signer.key;
+        storage_data.total_staked = 0u64;
+        storage_data.user_count = 0u64;
+        storage_data.rewards_per_token = rewards_per_token;
+        storage_data.is_initialized = true;
+        
+        storage_data.serialize(&mut &mut storage.data.borrow_mut()[..])?;
+        
+        msg!("Staking pool is initialized {:#?}", storage_data);
+        
+        Ok(())
+        }
+    }
